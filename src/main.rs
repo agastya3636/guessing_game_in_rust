8576fda2ca895b0060b27630f1cb45c6a3b57252
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-11-02 22:35:44 +0530

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,31 @@
 use std::io;
-
-const PLAYER_X:char = 'X';
-const PLAYER_O:char = 'O';
-const BOARD_SIZE:usize =3;
-
-type Board = [[char; BOARD_SIZE]; BOARD_SIZE];
-
-fn print_board(board: &Board) {
-    for i in 0..BOARD_SIZE {
-        for j in 0..BOARD_SIZE {
-            print!("{} ", board[i as usize][j as usize]);
-        }
-        println!();
-    }
-}
-
-fn initialize_board()->Board {
-    return [['-';BOARD_SIZE]; BOARD_SIZE];
-}
-fn check_winner(board: &Board, winner: &char)->bool {
-    for i in 0..BOARD_SIZE {
-        if board[i][0] == *winner && board[i][1] == *winner && board[i][2] == *winner {
-            return true;
-        }
-        if board[0][i] == *winner && board[1][i] == *winner && board[2][i] == *winner {
-            return true;
-        }
-    }
-    if board[0][0] == *winner && board[1][1] == *winner && board[2][2] == *winner {
-        return true;
-    }
-    if board[0][2] == *winner && board[1][1] == *winner && board[2][0] == *winner {
-        return true;
-    }
-    return false;
-}
-fn check_draw(board: &Board)->bool {
-    for i in 0..BOARD_SIZE {
-        for j in 0..BOARD_SIZE {
-            if board[i][j] == '-' {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-fn play_game(){
-    let mut board = initialize_board();
-    let mut player = PLAYER_X;
-    loop {
-        println!("Current board:");
-        print_board(&board);
-
-        println!("Enter row and column");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let mut parts = input.trim().split_whitespace();
-        let row:usize = parts.next().unwrap().parse().unwrap();
-        let col:usize = parts.next().unwrap().parse().unwrap();
-        if row >= BOARD_SIZE || col >= BOARD_SIZE {
-            println!("Invalid input. Try again");
-            continue;
-        }
-        if board[row][col] != '-' {
-            println!("Cell already taken. Try again");
-            continue;
-        }
-        board[row][col] = player;
-
-        if check_winner(&board, &player){
-            println!("Player {} wins", player);
-            break;
-        }
-        if check_draw(&board){
-            println!("Draw");
-            break;
-        }
-        player = if player==PLAYER_X{
-        PLAYER_O
-        }else{
-            PLAYER_X
-        };
-    }
-
-}
+use rand::prelude::*;
 
 fn main() {
-    play_game();
+    let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"];
+    let mut rng = rand::thread_rng();
+    loop {
+        println!("Welcome to the fruit guessing game!");
+        
+        let random_index = rng.gen_range(0..fruits.len());
+        let chosen_fruit = fruits[random_index];
+
+        println!("Guess the fruit!");
+        
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        if !fruits.contains(&guess.trim().to_lowercase().as_str()) {
+            println!("Fruit does not found!");
+            
+            continue;
+        } 
+        else if guess.trim().eq_ignore_ascii_case(chosen_fruit) {
+            println!("You guessed right!");
+            break;
+        } else {
+            println!("You guessed wrong! The fruit was {}", chosen_fruit);
+        }
+    }
 }
